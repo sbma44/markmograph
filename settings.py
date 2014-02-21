06e314f13495ee59e42cffa0518e775cf3b325b7
sbma44 <thomas.j.lee@gmail.com>
2014-02-21 11:47:51 -0500

    Adding code -- this is a personal fork of a repo, more or less, that's maintained centrally elsewhere

---
+++ settings.py
@@ -0,0 +1,7 @@
+AGENCY_TAG = 'sf-muni'
+PLAY_SOUND_IF_BUS_IS_LESS_THAN_THIS_MANY_SECONDS_AWAY = 60
+STOPS_AND_THEIR_SOUND_FILES = {
+	'14631': 'http://www.soundjay.com/misc/bell-ring-01.mp3',
+	'14632': 'http://www.davidmillington.net/trials/mobile-phone/Ring%20Tones/Monkey%20Island%20-%20Banjo%20Loop.wav'
+}
+WORKING_DIRECTORY = '/home/pi/Devel/markmograph'
